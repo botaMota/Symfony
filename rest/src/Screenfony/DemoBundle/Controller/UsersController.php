aa2a44b49076f670f618b99a1bd77802137d88cb
[a609848] El Moutaouakil BOUTAHIR <el-moutaouakil.boutahir@atos.net>
2016-03-31 09:07:00 +0100

    Initial commit

---
+++ rest/src/Screenfony/DemoBundle/Controller/UsersController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Screenfony\DemoBundle\Controller {
+
+    use Screenfony\DemoBundle\Entity\User;
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use FOS\RestBundle\Controller\Annotations\View;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter ;
+
+    class UsersController extends Controller
+    {
+        /**
+         *@return array
+         *@View()
+         */
+        public function getUsersAction()
+        {
+            $users = $this -> getDoctrine()->getRepository('ScreenfonyDemoBundle:User')->findAll();
+            return array('users' => $users);
+        }
+
+        /**
+         * @param User $user
+         *@return array
+         *@View()
+         * @ParamConverter("user", class="ScreenfonyDemoBundle:User")
+         */
+        public function getUserAction(User $user)
+        {
+            $users = $this -> getDoctrine()->getRepository('ScreenfonyDemoBundle:User')->findAll();
+            return array('users' => $user);
+        }
+    }
+}
